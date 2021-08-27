b363c02e75f29008ecab9fbc442f9943b8b8b3d7
Oscar Quiroz <65783932+oscarqochoa@users.noreply.github.com>
2021-08-27 15:55:30 -0500

    Autenticacion de usuario y renovacion de token

---
+++ middlewares/validar-campos.js
@@ -0,0 +1,21 @@
+const { validationResult } = require("express-validator");
+
+const validarCampos = (req, res, next) => {
+
+    const errores = validationResult(req);
+
+    console.log(errores);
+
+    if (!errores.isEmpty()) {
+        return res.status(400).json({
+            ok: false,
+            errors: errores.mapped()
+        });
+    }
+
+    next();
+}
+
+module.exports = {
+    validarCampos: validarCampos,
+}
