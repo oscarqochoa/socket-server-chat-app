cde69aa22013d8adc196c9a2c81f56aea632e2bc
Oscar Quiroz <65783932+oscarqochoa@users.noreply.github.com>
2021-09-01 12:50:18 -0500

    Anadir seguridad a los sockets y desarrollar controlador de usuarios

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,18 +1,28 @@
+const { comprobarJWT } = require('../helpers/jwt');
 const { io } = require('../index');
-
+const { usuarioConectado, usuarioDesconectado } = require('../controllers/socket');
 
 // Mensajes de Sockets
-io.on('connection', client => {
+io.on('connection', (client) => {
     console.log('Cliente conectado');
 
+    const [valido, uid] = comprobarJWT(client.handshake.headers['x-token']);
+
+    // Verificar autenticacion
+    if (!valido) { return client.disconnect(); }
+
+    // Cliente autenticado
+    usuarioConectado(uid);
+
+
     client.on('disconnect', () => {
-        console.log('Cliente desconectado');
+        usuarioDesconectado(uid);
     });
 
-    client.on('mensaje', ( payload ) => {
+    client.on('mensaje', (payload) => {
         console.log('Mensaje', payload);
 
-        io.emit( 'mensaje', { admin: 'Nuevo mensaje' } );
+        io.emit('mensaje', { admin: 'Nuevo mensaje' });
 
     });
 
